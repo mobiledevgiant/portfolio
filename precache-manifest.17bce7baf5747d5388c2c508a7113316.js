4d842972e0352ed75d173e9d4b63b8abd80bb832
Yevhenii R <yevheniirybalov90@gmail.com>
2023-01-18 22:17:25 +0100

    Updates

---
+++ precache-manifest.17bce7baf5747d5388c2c508a7113316.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0261c75a6eaa02c424f4735db25faa64",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "e0c413e5d3273fe81247",
+    "url": "/profile/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "8b9aa16a9356081b243c",
+    "url": "/profile/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "ba7092f325974893c728",
+    "url": "/profile/static/css/main.26a59920.chunk.css"
+  },
+  {
+    "revision": "a6b0b19d0d2c3c3b5ad5",
+    "url": "/profile/static/js/2.c133fdf6.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/profile/static/js/2.c133fdf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c413e5d3273fe81247",
+    "url": "/profile/static/js/3.72feead1.chunk.js"
+  },
+  {
+    "revision": "8b9aa16a9356081b243c",
+    "url": "/profile/static/js/4.8575ba78.chunk.js"
+  },
+  {
+    "revision": "ba7092f325974893c728",
+    "url": "/profile/static/js/main.6574912d.chunk.js"
+  },
+  {
+    "revision": "8da26c25b8e7459a26d3",
+    "url": "/profile/static/js/runtime-main.b769efc2.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/profile/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/profile/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/profile/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/profile/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "e3a410d54ad8fe15413dfe37eea090f2",
+    "url": "/profile/static/media/dcsLogo.e3a410d5.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/profile/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "a4427df08e89e79cbd158c828391a9ea",
+    "url": "/profile/static/media/fbeLogo.a4427df0.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/profile/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "5650d4fa50d156e12fd98bdb24ee5848",
+    "url": "/profile/static/media/helloLogo.5650d4fa.png"
+  },
+  {
+    "revision": "41c7ab5e9ec7c83a93890e4e3c670b93",
+    "url": "/profile/static/media/helloreportLogo.41c7ab5e.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/profile/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "b2f101ed45151c12adc9b26ff6115ad3",
+    "url": "/profile/static/media/miloscicLogo.b2f101ed.png"
+  },
+  {
+    "revision": "d4f9a940741ef2ce704d0e2cb54aeb72",
+    "url": "/profile/static/media/nuomLogo.d4f9a940.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/profile/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/profile/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "f0c7a39ecec4beda5499a8b2d110ce68",
+    "url": "/profile/static/media/talkitoutLogo.f0c7a39e.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/profile/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "16df3d6f658a1a563f0cb5f808e96c3d",
+    "url": "/profile/static/media/university.16df3d6f.jpeg"
+  },
+  {
+    "revision": "c50f92adfab5a4b16f489f84616a130a",
+    "url": "/profile/static/media/upliftLogo.c50f92ad.png"
+  }
+]);
